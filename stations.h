3729fd890c44e2300b34dd92c2a2d18179890de6
Abdullah <s-abdullah.yasser@zewailcity.edu.eg>
2023-12-29 13:07:14 +0200

    splitting into header and cpp file

--- stations.h
+++ stations.h
@@ -2,9 +2,10 @@
 #include "dataStructures/NoramlQueue.h"
 #include "dataStructures/LinkedListp.h"
 #include "dataStructures/PriorityQueue.h"
-#include <iostream>
 #include "Buses.h"
 #include "Passenger.h"
+
+#include <iostream>
 using namespace std;
 template <typename N>
 class Nodestation{
@@ -42,27 +43,27 @@ public :
 
 
     Nodestation(){
-        Snumber  = 0;
+    Snumber  = 0;
     }
 
     void PrintStationInfo(){ 
-        cout<<"Station Number: "<<Snumber<<endl;
-        cout<<"Forward NP list: ";
-        NP.PrintList();
-        cout<<"Backword NP list: ";
-        BNP.PrintList();
-        cout<<"Forward WP list: ";
-        WP.printQueue();
-        cout<<"Backword WP list: ";
-        BWP.printQueue();
-        cout<<"Forward SP Queue: ";
-        SP.printQueue();
-        cout<<"Backword SP Queue: ";
-        BSP.printQueue();
-        cout << "Garage: " << endl;
-        cout <<"    Normal Buses: "<<Ngarage.count() <<endl; 
-        cout <<"    Wheel Buses: " <<Wgarage.count() << endl;
-        cout<<"-------------------------------------------------"<<endl;
+    cout<<"Station Number: "<<Snumber<<endl;
+    cout<<"Forward NP list: ";
+    NP.PrintList();
+    cout<<"Backword NP list: ";
+    BNP.PrintList();
+    cout<<"Forward WP list: ";
+    WP.printQueue();
+    cout<<"Backword WP list: ";
+    BWP.printQueue();
+    cout<<"Forward SP Queue: ";
+    SP.printQueue();
+    cout<<"Backword SP Queue: ";
+    BSP.printQueue();
+    cout << "Garage: " << endl;
+    cout <<"    Normal Buses: "<<Ngarage.count() <<endl; 
+    cout <<"    Wheel Buses: " <<Wgarage.count() << endl;
+    cout<<"-------------------------------------------------"<<endl;
     }
 
 };
@@ -74,91 +75,20 @@ public:
     int size;
     Nodestation<T>* list;
     
-    Stations() {
-        size=0;
-        list=nullptr;
-    }
-    Stations(int size){
-        list = new Nodestation<T>[size+1];
-    }
+    Stations();
+    Stations(int size);
 
-    void addStationsByNumber(int numberOfStations)
-    {
-        Nodestation<T>* newArray=new Nodestation<T>[numberOfStations+1]{};
-        delete[] list;
-        list=newArray;
-        size=numberOfStations+1;
-        for (int i =0;i<=numberOfStations;i++){
-            list[i].setSnumber(i);
-        }
-    }
+    void addStationsByNumber(int numberOfStations);
+    void display(int size);
+    void PrintAllStations(int size);
+    void addPassenger(Passenger* passenger);
+    void storeNBus(Bus* bus);
+    void storeWBus(Bus* bus);
+    int passDirection(Passenger* passenger);
+    void RemovePassenger(Passenger* passenger, int stationNumber);
+    int getStationSize();
 
- 
-    void display(int size){
-        for(int i=0;i<size;i++){
-            list[i].Snumber=i;
-            cout<<list[i].Snumber<<endl;
-        }
-    }
-
-    void PrintAllStations(int size) 
-    {
-        for (int i=0;i<size;i++)
-        {
-            list[i].PrintStationInfo();
-        }
-    }
-
-    void addPassenger(Passenger* passenger){
-        int startStation = passenger->getStartStation();
-        string passengerType = passenger->getType();
-        int passDrection = passDirection(passenger);
-       
-        if (passengerType == "NP") {
-            if(passDrection==0) list[startStation].getNP()->Insert(passenger);
-            else list[startStation].getBNP()->Insert(passenger);
-        } else if (passengerType == "WP") {
-            if(passDrection==0) list[startStation].getWP()->enqueue(passenger);
-            else list[startStation].getBWP()->enqueue(passenger);
-        }else if (passengerType == "SP") {
-            if(passDrection==0) list[startStation].getSP()->enqueue(passenger);
-            else list[startStation].getBSP()->enqueue(passenger);        
-            }
-
-    }
-
-    void storeNBus(Bus* bus)
-    {
-        list[0].getNgarage()->enqueue(bus);
-
-    }
-
-    void storeWBus(Bus* bus)
-    {
-            list[0].getWgarage()->enqueue(bus);
-
-    }
-
-
-    int passDirection(Passenger* passenger){
-        int start = passenger->getStartStation();
-        int end = passenger->getEndStation();
-        if ((end-start)>0) return 0;
-        else return 1;
-    }
-    
-    void RemovePassenger(Passenger* passenger, int stationNumber){ // This function is used for the leave evvent
-        int passDrection = passDirection(passenger);
-        //int startStation = passenger->getStartStation();
-        if(passDrection==0) list[stationNumber].getNP()->RemovePassenger(passenger);
-        else list[stationNumber].getBNP()->RemovePassenger(passenger);
-    }
-
-    int getStationSize(){
-        return size;
-    }
-
-    // void printQueue() const {
+    // void printQueue(Queue* queue) const {
     //     PriorityNode<T>* current = front;
     //     while (current != nullptr) {
     //         cout << "[ " << current->getItem()->getID() << " ]";
@@ -169,8 +99,6 @@ public:
     //     cout << "*\n";
     // }
 
-    // void moveToNextStation() {
-    //     currentStation++;
-    // }
 
 };
+#include "Stations.cpp"
